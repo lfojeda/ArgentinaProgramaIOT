53b182ad71d9aa7b09d37c517434ca488a2bb10b
Luis Fernando Ojeda <lfojeda@jusmisiones.gov.ar>
2023-07-07 17:45:07 -0300

    Capitulo 2 - Ejercicio 2

---
+++ main.py
@@ -0,0 +1,22 @@
+# Luis Fernando Ojeda  2023
+ 
+from machine import Pin
+import time
+
+print("Esperando pulsador")
+
+sw = Pin(23, Pin.IN)
+led = Pin(2, Pin.OUT)
+
+contador=0
+while True:
+    if sw.value():
+        contador+=1
+        print(contador)     
+    else:
+        if contador>3:
+            print("Pulsacion larga")
+        if 1<=contador<=2:
+            print("Pulsacion corta")  
+        contador=0    
+    time.sleep_ms(200)
