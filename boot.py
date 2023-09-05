7349aa0e4e25726582e35556d67c5133b48d2462
Luis Fernando Ojeda <lfojeda@jusmisiones.gov.ar>
2023-09-05 01:35:51 -0300

    Se envian modificaciones

---
+++ boot.py
@@ -0,0 +1,12 @@
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('Wokwi-GUEST', '')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+
+do_connect()
---
+++ settings.py
@@ -0,0 +1,3 @@
+SSID="Wokwi-GUEST"
+PASS_WLAN=""
+SERVIDOR_MQTT="iotunam.duckdns.org"
