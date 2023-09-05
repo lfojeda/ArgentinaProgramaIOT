e9be949f97308e96c0f0443e6789315d7f7b8059
Luis Fernando Ojeda <lfojeda@jusmisiones.gov.ar>
2023-09-05 00:02:09 -0300

    se sube programa terminado

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 # Luis Fernando Ojeda - 2023
-from machine import Pin
+from machine import Pin, Timer
 import dht
 import time
 import json
-from collections import OrderedDict
 
 sw = Pin(23, Pin.IN)
 led = Pin(2, Pin.OUT)
 d = dht.DHT22(Pin(25))
-contador=0
+print("Esperando la primera pulsación...")
+contador = 0
+estado = False
+temperaturas = []
+humedades = []
+
+def alternar(pin):
+    global contador, estado
+    if sw.value():            
+            led.value(True)
+            contador += 1
+            print(f'Pulsación {contador}')
+            d.measure()
+            temperatura = d.temperature()
+            humedad = d.humidity()
+            if contador == 1:
+                # Primera pulsación: tomar temperatura y humedad
+                print(f'Temperatura: {temperatura}°C, Humedad: {humedad}%')
+                temperaturas.append(temperatura)
+                humedades.append(humedad)
+            elif contador == 2:
+                # Segunda pulsación: tomar temperatura y humedad y
+                # calcular el promedio y imprimirlo
+                print(f'Temperatura: {temperatura}°C, Humedad: {humedad}%')
+                temperaturas.append(temperatura)
+                humedades.append(humedad)
+                if temperaturas and humedades:
+                    promedio_temp = sum(temperaturas) / len(temperaturas)
+                    promedio_hum = sum(humedades) / len(humedades)
+                    print(f'Promedio de Temperatura: {promedio_temp:.2f}°C, Promedio de Humedad: {promedio_hum:.2f}%')
+                else:
+                    print("No se tomaron lecturas en la primera pulsación.")
+                contador = 0
+                temperaturas.clear()
+                humedades.clear()
+            led.value(False)
+
+
+timer1 = Timer(1)
+timer1.init(period=50, mode=Timer.PERIODIC, callback=alternar)
 
 while True:
-    print(contador)
-    contador+=1
-    try:
-        d.measure()
-        temperatura=d.temperature()
-        humedad=d.humidity()
-        datos=json.dumps(OrderedDict([
-            ('temperatura',temperatura),
-            ('humedad',humedad)
-        ]))
-        print(datos)
-    except OSError as e:
-        print("sin sensor")
-
-    time.sleep(20)
+    time.sleep(1)
