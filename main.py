13ded06af16e7aad1e09e4c31eecae6bd82753a5
Luis Fernando Ojeda <lfojeda@jusmisiones.gov.ar>
2023-09-05 00:00:21 -0300

    Se suben cambios

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-# Luis Fernando Ojeda  2023
-
+# Luis Fernando Ojeda - 2023
 from machine import Pin
+import dht
 import time
-print("Esperando pulsador")
+import json
+from collections import OrderedDict
 
 sw = Pin(23, Pin.IN)
 led = Pin(2, Pin.OUT)
-
+d = dht.DHT22(Pin(25))
 contador=0
+
 while True:
-    if sw.value():
-        contador+=1
-        if contador % 10 == 0:
-            led.value(not led.value())
-            contador=0
-        print(contador)
-        time.sleep_ms(200)
+    print(contador)
+    contador+=1
+    try:
+        d.measure()
+        temperatura=d.temperature()
+        humedad=d.humidity()
+        datos=json.dumps(OrderedDict([
+            ('temperatura',temperatura),
+            ('humedad',humedad)
+        ]))
+        print(datos)
+    except OSError as e:
+        print("sin sensor")
+
+    time.sleep(20)
