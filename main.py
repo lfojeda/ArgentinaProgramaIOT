a44e109c8414e899f7a812bca53724b5417b4635
Luis Fernando Ojeda <lfojeda@jusmisiones.gov.ar>
2023-09-07 08:51:46 -0300

    Se agrega código para enviar temperaturas al broker de la Unam cuando la temperatura excede un rango

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# Luis Fernando Ojeda - 2023
+# Luis Fernando Ojeda 2023
+
 from machine import Pin, Timer, unique_id
 import dht
 import time
@@ -16,8 +17,12 @@ mqtt = MQTTClient(CLIENT_ID, SERVIDOR_MQTT,
 led = Pin(2, Pin.OUT)
 d = dht.DHT22(Pin(25))
 contador = 0
+max_temp=50
+min_temp=20
+registrar=True
+enviar=False
 
-def heartbeat(nada):
+def heartbeat(none):
     global contador
     if contador > 5:
         pulsos.deinit()
@@ -27,11 +32,15 @@ def heartbeat(nada):
     contador += 1
   
 def transmitir(pin):
-    print("publicando")
-    mqtt.connect()
-    mqtt.publish(f"ap/{CLIENT_ID}",datos)
-    mqtt.disconnect()
-    pulsos.init(period=150, mode=Timer.PERIODIC, callback=heartbeat)
+    global enviar,registrar
+    if enviar:
+        print("publicando")
+        mqtt.connect()
+        mqtt.publish(f"ap/{CLIENT_ID}",datos)
+        mqtt.disconnect()
+        pulsos.init(period=150, mode=Timer.PERIODIC, callback=heartbeat)
+        enviar=False
+        registrar=False
 
 publicar = Timer(0)
 publicar.init(period=30000, mode=Timer.PERIODIC, callback=transmitir)
@@ -41,12 +50,15 @@ while True:
     try:
         d.measure()
         temperatura = d.temperature()
-        humedad = d.humidity()
-        datos = json.dumps(OrderedDict([
-            ('temperatura',temperatura),
-            ('humedad',humedad)
-        ]))
-        print(datos)
+        if temperatura>max_temp and registrar:
+            datos = json.dumps(OrderedDict([('temperatura',temperatura)]))
+            registrar=False
+            enviar=True
+            print(datos)
+        if temperatura<min_temp and not registrar:
+            registrar=True
+            print("Temperatura por debajo de la mínima se activa zona de envio")
+            
     except OSError as e:
         print("sin sensor")
     time.sleep(5)
